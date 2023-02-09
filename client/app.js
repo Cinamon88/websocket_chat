d31941abeec77d22db48f93a7c81b296d253d399
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-09 21:55:45 +0100

    add addMessage and remove text

--- client/app.js
+++ client/app.js
@@ -1,23 +1,23 @@
 // HTML references
 
-const loginForm = document.querySelector('#welcome-form');
-const messagesSection = document.querySelector('#messages-section');
-const messagesList = document.querySelector('#messages-list');
-const addMessagesForm = document.querySelector('#add-messages-form');
-const userNameInput = document.querySelector('#username');
-const messageContentInput = document.querySelector('#message-content')
+const loginForm = document.querySelector("#welcome-form");
+const messagesSection = document.querySelector("#messages-section");
+const messagesList = document.querySelector("#messages-list");
+const addMessagesForm = document.querySelector("#add-messages-form");
+const userNameInput = document.querySelector("#username");
+const messageContentInput = document.querySelector("#message-content")
 
 // Global
-let userName = '';
+let userName = "";
 
 const login = (e) => {
     if (userNameInput.value.length > 0) {
         userName = userNameInput.value;
-        loginForm.classList.remove('show');
-        messagesSection.classList.add('show');
-        addMessagesForm.classList.add('show');
+        loginForm.classList.remove("show");
+        messagesSection.classList.add("show");
+        addMessagesForm.classList.add("show");
     } else {
-        alert('Please enter a username');
+        alert("Please enter a username");
     }
 };
 
@@ -27,9 +27,23 @@ const sendMessage = (e) => {
         addMessage(userName, messageContentInput.value);
         messageContentInput.value = "";
     } else {
-        alert('Please enter a message');
+        alert("Please enter a message");
     }
 };
 
-loginForm.addEventListener('submit', (e) => login(e));
-addMessagesForm.addEventListener('submit', (e) => sendMessage(e));
+function addMessage(author, content) {
+    const message = document.createElement("li");
+    message.classList.add("message");
+    message.classList.add("message--received");
+    if(author === userName) message.classList.add("message--self");
+    message.innerHTML = `
+        <h3 class="message__author">${userName === author ? "You" : author }</h3>
+        <div class="message__content">
+            ${content}
+        <div>
+    `;
+    messagesList.appendChild(message);
+}
+
+loginForm.addEventListener("submit", (e) => login(e));
+addMessagesForm.addEventListener("submit", (e) => sendMessage(e));
